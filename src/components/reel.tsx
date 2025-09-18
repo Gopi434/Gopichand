a264a320eedb99b0b1f6ae7d35b3bbf2d1b2a737
Gopichand <gopichandt22@gmail.com>
2025-09-18 05:47:21 +0000

    there should be more space between white border and circular svg image.

--- src/components/reel.tsx
+++ src/components/reel.tsx
@@ -11,7 +11,7 @@ const Reel = () => {
   return (
     <Dialog open={open} onOpenChange={setOpen}>
       <div className="relative flex h-[140px] w-[140px] items-center justify-center sm:h-[200px] sm:w-[200px] md:h-[250px] md:w-[250px]">
-        <div className="absolute inset-0 animate-spin-slow z-10 p-4">
+        <div className="absolute inset-0 animate-spin-slow z-10 p-6">
           <Image
             src="https://raw.githubusercontent.com/Gopi434/Media/e46057285648f43e82da418a13e52d48c997e521/text-ring.svg"
             alt="Watch showreel text"
@@ -20,7 +20,7 @@ const Reel = () => {
           />
         </div>
 
-        <div className="relative h-full w-full rounded-full shadow-2xl backdrop-blur-lg border-[0.3rem] border-white p-[2%]">
+        <div className="relative h-full w-full rounded-full shadow-2xl backdrop-blur-lg border-[0.15rem] border-white p-[2%]">
           <div
             className="absolute inset-0 rounded-full"
           >
