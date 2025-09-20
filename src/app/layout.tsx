78bbe7017f74c301463ec8029298a572957ec0e3
Gopichand <gopichandt22@gmail.com>
2025-09-20 04:55:05 +0000

    Lates build

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import { Toaster } from '@/components/ui/toaster';
 import Script from 'next/script';
 
 export const metadata: Metadata = {
-  title: ' Gopichand | Product Designer',
+  title: ' Gopichand | Product Designer | Multidisciplinary in Grahic, Motion,Web, 3D and Video editing',
   description: 'Personal portfolio website of Gopichand, Including showreel and works',
 };
 
@@ -16,7 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <head>
-        <link rel="icon" href="https://raw.githubusercontent.com/Gopi434/Media/620377c580f802bf1195061dc6b5926071e2e125/FavIcon-02.svg" type="image/svg+xml" />
+        <link rel="icon" href="https://raw.githubusercontent.com/Gopi434/Media/f3e9eff33e55dffdab065d9182826a2635ce3be8/FavIcon.svg" type="image/svg+xml" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet" />
