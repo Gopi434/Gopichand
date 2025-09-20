46937cb63b082a454b05a53cbea4ae40b0d72111
Gopichand <gopichandt22@gmail.com>
2025-09-20 05:16:02 +0000

    Latest build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -36,7 +36,7 @@ export default function Home() {
       <AnimatedGridBackground isReelHovered={isReelHovered} isModalOpen={isModalOpen} isInteracted={isInteracted} />
 
       <header className={cn(
-        "absolute top-0 left-0 right-0 z-20 flex justify-center px-4 pt-8 md:px-8 md:pt-4 transition-transform duration-500 ease-in-out",
+        "absolute top-0 left-0 right-0 z-20 flex justify-center px-4 pt-12 md:px-8 md:pt-3 transition-transform duration-500 ease-in-out",
         isMobile && isInteracted && "translate-y-8"
       )}>
        
@@ -44,7 +44,7 @@ export default function Home() {
         <Image
           src="https://raw.githubusercontent.com/Gopi434/Media/447711a7a9152b349829c59f6c84920051b9a755/Logo.svg"
           alt="Gopichand Logo"
-          width={180}
+          width={220}
           height={97}
           className="md:w-[250px] md:h-[135px]"
         />
@@ -63,7 +63,7 @@ export default function Home() {
       </div>
 
       <footer className={cn(
-        "absolute bottom-0 left-0 right-0 z-20 flex justify-center px-4 pb-8 md:px-8 md:pt-8 md:pb-[4%] transition-transform duration-500 ease-in-out",
+        "absolute bottom-0 left-0 right-0 z-20 flex justify-center px-4 pb-14 md:px-8 md:pt-8 md:pb-[4%] transition-transform duration-500 ease-in-out",
         isMobile && isInteracted && "-translate-y-8"
       )}>
         <Button asChild variant="default" className="h-12 px-2 bg-primary text-primary-foreground hover:bg-primary/90 rounded-full text-base font-bold">
